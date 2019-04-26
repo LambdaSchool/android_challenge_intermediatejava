f8125b8551f78c569ba182e474fd554dbeda3755
Israel dela Cruz <igdcruz42@gmail.com>
2019-04-26 10:10:22 -0500

    created project

---
+++ Sprint6/app/src/main/java/com/example/israel/sprint6/OnDoubleClickListener.java
@@ -0,0 +1,10 @@
+package com.example.israel.sprint6;
+
+import android.view.View;
+
+public class OnDoubleClickListener implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+        
+    }
+}
