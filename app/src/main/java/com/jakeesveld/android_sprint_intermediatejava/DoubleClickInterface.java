a973f40e2d7874cf30f4d77837a7a28920594d33
Jake Esveld <thejakeesveld@gmail.com>
2019-04-26 08:15:10 -0700

    add interfaces for doubleclick and doubleclicklistener

---
+++ app/src/main/java/com/jakeesveld/android_sprint_intermediatejava/DoubleClickInterface.java
@@ -0,0 +1,5 @@
+package com.jakeesveld.android_sprint_intermediatejava;
+
+public interface DoubleClickInterface {
+    void setOnClickListener(DoubleClickListener onClickListener);
+}
---
+++ app/src/main/java/com/jakeesveld/android_sprint_intermediatejava/DoubleClickListener.java
@@ -0,0 +1,8 @@
+package com.jakeesveld.android_sprint_intermediatejava;
+
+import android.view.View;
+
+public interface DoubleClickListener extends View.OnClickListener {
+    void singleClick();
+    void doubleClick();
+}
